197c654f6c51dc97a88d667969c00bd0a2c35d11
Federico Verón <111612837+Fedeva1996@users.noreply.github.com>
2023-04-27 15:49:19 -0400

    ejercicio 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const part1 = 'Fundamentals of React'
-const exercises1 = 10
-const part2 = 'Using props to pass data'
-const exercises2 = 7
-const part3 = 'State of a component'
-const exercises3 = 14
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-const Part = (props) => {
-  return <><p>{ props.part } { props.exercises }</p></>;
-}
-const Content = (props) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+    <p>{course}</p>
+    <p>{part1.name} {part1.exercises}</p>
+    <p>{part2.name} {part2.exercises}</p>
+    <p>{part3.name} {part3.exercises}</p>
     </div>
-  );
-}
-
-const App = () => {
-
-  return (
-    <Content />
   )
 }
 
