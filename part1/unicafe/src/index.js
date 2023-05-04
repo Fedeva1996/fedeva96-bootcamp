52b6feeece64824888d6e9b9d761e37bc4f19944
Federico Verón <111612837+Fedeva1996@users.noreply.github.com>
2023-05-04 16:46:35 -0400

    ejercicio 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,11 +5,14 @@ const Button = ({ onClick, text }) => (
     {text}
   </button>
 )
-const Stats = ({good, neutral, bad}) => (
+const Stats = ({ good, neutral, bad, all, average }) => (
   <div>
     <p>Good: {good}</p>
     <p>Neutral: {neutral}</p>
     <p>Bad: {bad}</p>
+    <p>All: {all}</p>
+    <p>Average: {(good + neutral + bad) / 3}</p>
+    <p>Positive: {(good * 100) / all}%</p>
   </div>
 )
 const App = () => {
@@ -17,15 +20,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
   const handleBad = () => {
     setBad(bad + 1)
-  }  
+    setAll(all + 1)
+    setAverage(average - 1)
+  }
   const handleNeutral = () => {
     setNeutral(neutral + 1)
-  }  
+    setAll(all + 1)
+  }
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setAverage(average + 1)
   }
   return (
     <div>
@@ -34,7 +43,13 @@ const App = () => {
       <Button onClick={handleNeutral} text="neutral" />
       <Button onClick={handleGood} text="bad" />
       <h1>Statistics</h1>
-      <Stats good={good} neutral={neutral} bad={bad} />
+      <Stats
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+      />
     </div>
   )
 }
