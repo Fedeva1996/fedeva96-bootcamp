15d2777dacaf24302fb21d320b2f2d272455f009
Federico Verón <111612837+Fedeva1996@users.noreply.github.com>
2023-05-04 16:32:42 -0400

    ejercicio 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,19 +1,44 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+const Stats = ({good, neutral, bad}) => (
+  <div>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+  </div>
+)
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleBad = () => {
+    setBad(bad + 1)
+  }  
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }  
+  const handleGood = () => {
+    setGood(good + 1)
+  }
   return (
     <div>
-      code here
+      <h1>Give feedback</h1>
+      <Button onClick={handleBad} text="bad" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleGood} text="bad" />
+      <h1>Statistics</h1>
+      <Stats good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
